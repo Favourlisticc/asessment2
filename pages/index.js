8ba6ab1015a8783a61eb0e4c3fb8491a47b968fb
Lomoee <favoursunday600@gmail.com>
2024-04-23 23:34:07 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div id="" className="animate__animated animate__zoomIn animate__delay-1s">
+    <div id="" className="">
 
 
       <div id="container06" className="style7 container columns full screen">
@@ -52,7 +52,7 @@ export default function Home() {
     </div>
 
 
-    <h2 id="text09" class="style5 max-sm:mt-10 max-sm:text-center animate__animated animate__fadeInUp animate__delay-1s" style={{opacity: 1, transform: "none"}}>This might sound familiar to you:</h2>
+    <h2 id="text09" class="style5 max-sm:mt-10 max-sm:text-center   " style={{opacity: 1, transform: "none"}}>This might sound familiar to you:</h2>
 
     <div id="list02" className="style1 list ml-44 max-sm:ml-10 max-sm:mr-10 max-sm:mt-5">
       <ol>
@@ -71,7 +71,7 @@ export default function Home() {
     <div id="" className="style8 container w-2/3 ml-48 max-sm:w-96 max-sm:ml-6 ">
       <div className="wrapper">
         <div className="inner" data-onvisible-trigger="1">
-          <h2 id="text08" className="style5 animate__animated animate__fadeInUp animate__delay-1s" style={{ opacity: 1, transform: 'none' }}>Except there’s a problem:</h2>
+          <h2 id="text08" className="style5   " style={{ opacity: 1, transform: 'none' }}>Except there’s a problem:</h2>
           <div id="list03" className="style1 list">
             <ol>
               <li>
@@ -103,8 +103,8 @@ export default function Home() {
       </div>
     </div>
 
-    <h2 id="" className=" text-white text-center text-3xl animate__animated animate__fadeInUp animate__delay-1s" style={{opacity: 1, transform: "none"}}>That's where I come in:</h2>
-    <p id="" class="text-gray-300 text-center text-xl font-light mx-72 max-sm:mx-0 animate__animated animate__fadeInUp animate__delay-2s" style={{opacity: 1}}>I take care of your background operations, advise you, and support you - allowing you to scale and spend less time working on your business.</p>
+    <h2 id="" className=" text-white text-center text-3xl   " style={{opacity: 1, transform: "none"}}>That's where I come in:</h2>
+    <p id="" class="text-gray-300 text-center text-xl font-light mx-72 max-sm:mx-0   animate__delay-2s" style={{opacity: 1}}>I take care of your background operations, advise you, and support you - allowing you to scale and spend less time working on your business.</p>
 
     <div id="container08" className="style8 container default  w-2/3 ml-48 max-sm:w-96 max-sm:ml-6">
       <div className="wrapper">
@@ -134,7 +134,7 @@ export default function Home() {
 
         <div className="flex mb-10 max-sm:flex-col-reverse">
 
-          <div className="w-1/2 max-sm:w-full animate__animated animate__slideInLeft animate__delay-1s">
+          <div className="w-1/2 max-sm:w-full  animate__slideInLeft ">
             <div id="" className="" style={{ opacity: 1, transform: 'none' }}>
               <span className="frame">
                 <Image src={day2img} alt="" className="w-52 ml-40 max-sm:ml-28 max-sm:mt-20 " onContextMenu={() => false} onDragStart={() => false} />
@@ -145,7 +145,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="w-2/3 mt-20 max-sm:mt-5 max-sm:w-full animate__animated animate__bounceInUp animate__delay-1s">
+          <div className="w-2/3 mt-20 max-sm:mt-5 max-sm:w-full  animate__bounceInUp ">
             <h3 id="" className="style3 text-xl" style={{ opacity: 1, transform: 'none', fontSize: "25px" }}>
               <mark>$11,315</mark> in recurring revenue in just <mark>20 days</mark>
             </h3>
@@ -165,7 +165,7 @@ export default function Home() {
             <h2 id="text03" className="style3" style={{ opacity: 1, transform: 'none' }}>
               About <mark>My Paid Community</mark>
             </h2>
-            <p id="text12" className="style4 animate__animated animate__zoomInDown animate__delay-2s" style={{ opacity: 1 }}>
+            <p id="text12" className="style4  animate__zoomInDown animate__delay-2s" style={{ opacity: 1 }}>
               <span className="p">I worked for 5+ years at a fast-paced startup as a Creator Success Manager, helping 75+ creators &amp; artists build audiences, monetize their content, and grow their businesses.</span>
               <span className="p">Now I help coaches &amp; creators with custom subscription-based communities and high-ticket digital products.</span>
             </p>
@@ -174,7 +174,7 @@ export default function Home() {
             </ul>
           </div>
           <div>
-            <div id="image06" className="style5 image max-sm:ml-20 animate__animated animate__slideInRight animate__delay-1s" style={{ opacity: 1, transform: 'none' }}>
+            <div id="image06" className="style5 image max-sm:ml-20  animate__slideInRight " style={{ opacity: 1, transform: 'none' }}>
               <span className="frame"><Image src={man} alt="" onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()} /></span>
             </div>
           </div>
@@ -186,27 +186,27 @@ export default function Home() {
 
     <ul id="icons01" className="style1 icons">
     <li style={{ opacity: 1, transform: 'none' }}>
-      <a className="n01 animate__animated animate__zoomInDown animate__delay-1s" href="https://twitter.com/JanisMjartans" aria-label="Twitter">
+      <a className="n01  animate__zoomInDown " href="https://twitter.com/JanisMjartans" aria-label="Twitter">
       <svg fill="currentColor" viewBox="0 0 24 24" class="w-5 h-5" aria-hidden="true"><path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z"></path></svg>
         <span className="label">Twitter</span>
       </a>
     </li>
     <li style={{ opacity: 1, transform: 'none' }}>
-      <a className="n02 animate__animated animate__zoomInDown animate__delay-2s" href="https://www.linkedin.com/in/janismjartans/" aria-label="LinkedIn">
+      <a className="n02  animate__zoomInDown animate__delay-2s" href="https://www.linkedin.com/in/janismjartans/" aria-label="LinkedIn">
       <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
 
         <span className="label">LinkedIn</span>
       </a>
     </li>
     <li style={{ opacity: 1, transform: 'none' }}>
-      <a className="n03 animate__animated animate__zoomInDown animate__delay-3s" href="mailto:janis@example.com" aria-label="Email">
+      <a className="n03  animate__zoomInDown animate__delay-3s" href="mailto:janis@example.com" aria-label="Email">
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path d="M17 3.5H7c-3 0-5 1.5-5 5v7c0 3.5 2 5 5 5h10c3 0 5-1.5 5-5v-7c0-3.5-2-5-5-5Zm.47 6.09-3.13 2.5c-.66.53-1.5.79-2.34.79-.84 0-1.69-.26-2.34-.79l-3.13-2.5a.77.77 0 0 1-.12-1.06c.26-.32.73-.38 1.05-.12l3.13 2.5c.76.61 2.05.61 2.81 0l3.13-2.5c.32-.26.8-.21 1.05.12.26.32.21.8-.11 1.06Z" fill="#ffffff"></path></svg>
         <span className="label">Email</span>
       </a>
     </li>
   </ul>
 
-  <p id="text04" class="style1 animate__animated animate__fadeIn  animate__delay-1s " style={{opacity: 1}}>hello@mypaidcommunity.com</p>
+  <p id="text04" class="style1  animate__fadeIn   " style={{opacity: 1}}>hello@mypaidcommunity.com</p>
   <p id="text43" class="style1" style={{opacity: 1}}>© 2024 My Paid Community. All rights reserved.</p>
 
   <button className="z-50 fixed md:bottom-6 bottom-4 flex h-16 origin-center bg-red-50 transform cursor-pointer items-center rounded-full py-4 px-6 text-base outline-none drop-shadow-md transition focus:outline-none focus:ring-4 focus:ring-gray-600 focus:ring-opacity-50 active:scale-95 md:right-10 right-4"
